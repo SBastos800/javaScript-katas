6c368cf05dbd651a160409e36790c494b35e7224
SBastos800 <soniavsbastos@gmail.com>
2020-05-13 17:14:12 +0100

    reviewed katas

--- solutions/q14.ts
+++ solutions/q14.ts
@@ -1,9 +1,8 @@
 // How do you swap two numbers without using a third variable
 
 export let swapWithoutThirdVariable = (a: number, b: number) => {
-    var a = 1,
+    a = 1,
     b = 2;
 
-[a, b] = [b, a];
-
+    [a, b] = [b, a];
 }
